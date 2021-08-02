0f5385526fe09f6807c8fc0563cba7bf04b2d370
Sreedhar <sreedhar@Sreedhars-MacBook-Pro.local>
2021-08-02 23:54:15 +0530

    Snackbar changes

--- src/common/snackbar/SnackBarNotification.js
+++ src/common/snackbar/SnackBarNotification.js
@@ -2,19 +2,19 @@ import { IconButton, Snackbar } from '@material-ui/core';
 import { Close } from '@material-ui/icons';
 import React from 'react';
 
-export default function SnackBarNotification(props){
-    return(
-        <Snackbar open={props.open} message ={props.message} onClose={props.notificationCloseHandler} autoHideDuration={1000} anchorOrigin={{
+export default function SnackBarNotification(props) {
+    return (
+        <Snackbar open={props.open} message={props.message} onClose={props.notificationCloseHandler} autoHideDuration={1000} anchorOrigin={{
             vertical: 'bottom',
             horizontal: 'left',
-        }} 
-        action={
-            <React.Fragment>
-                <IconButton size="small" aria-label="close" color="inherit" onClick={props.notificationCloseHandler}>
-                    <Close fontSize="small" />
-                </IconButton>
-            </React.Fragment>
-        }
+        }}
+            action={
+                <React.Fragment>
+                    <IconButton size="small" aria-label="close" color="inherit" onClick={props.notificationCloseHandler}>
+                        <Close fontSize="small" />
+                    </IconButton>
+                </React.Fragment>
+            }
         />
     )
 }
