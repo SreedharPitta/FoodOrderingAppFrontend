625853c1e040a68c1f4c05cf8962a8afc31d3a59
Sreedhar <sreedhar@Sreedhars-MacBook-Pro.local>
2021-08-02 07:55:52 +0530

    Header changes

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,43 +3,61 @@ import { Component } from 'react';
 import './Header.css';
 import { withStyles } from '@material-ui/styles';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
+import SearchIcon from '@material-ui/icons/Search';
+import Search from '@material-ui/icons/Search';
 
-const styles = theme =>({
-    appheader : {
+const styles = theme => ({
+    appheader: {
         height: '65px',
         backgroundColor: '#253238',
         display: 'inline-flex',
         width: '100%',
         alignItems: 'center',
     },
-    appHeaderContent : {
-        padding : '0px 15px'
+    appHeaderContent: {
+        padding: '0px 15px'
     },
-    appLogo : {
-        color : '#fff',
-        fontSize : '30px'
+    appLogo: {
+        color: '#fff',
+        fontSize: '30px'
     }
-    
+
 })
 
-class Header extends Component{
-    constructor(){
+class Header extends Component {
+    constructor() {
         super();
         this.state = {
-            loggedIn : sessionStorage.getItem("access-token") === null ? false : true
+            loggedIn: sessionStorage.getItem("access-token") === null ? false : true
         }
     }
-    render(){
-        const {classes} = this.props
-        return(
+    render() {
+        const { classes } = this.props
+        return (
             <div className={classes.appheader}>
-            <div className ={classes.appHeaderContent}>
-            <div className={classes.appLogoHolder}>
-                    <FastfoodIcon className={classes.appLogo}/>
-            </div>
-            </div>
+                <div className={classes.appHeaderContent}>
+                    <div className={classes.appLogoHolder}>
+                        <FastfoodIcon className={classes.appLogo} />
+                    </div>
+                    {this.props.showMenuItems === "home" && (
+                        <div className={classes.headerHomeRight}>
+                            <div className={classes.searchHolder}>
+                                <Search />
+                            </div>
+                        </div>
+                    )}
+                    {!this.state.loggedIn && (
+                        <div>
+                        </div>
+                    )}
+                    {this.state.loggedIn && (
+                        <div>
+
+                        </div>
+                    )}
+                </div>
             </div>
         )
     }
 }
-export default withStyles(styles) (Header);
+export default withStyles(styles)(Header);
