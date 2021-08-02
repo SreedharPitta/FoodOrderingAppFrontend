2c4109af125abbaa0511d4b56e1de662ea4fcade
Sreedhar <sreedhar@Sreedhars-MacBook-Pro.local>
2021-08-02 23:29:31 +0530

    Profile changes

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { Component } from 'react';
 import './Profile.css';
 import { withStyles } from '@material-ui/styles';
+import Header from '../../common/header/Header';
 
-const styles = theme =>({
+const styles = theme => ({
 
 })
 
-class Profile extends Component{
-    render(){
+class Profile extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    render() {
+        if (sessionStorage.getItem('access-token') === null) {
+            this.props.history.push('/');
+        }
         return (
-            <div>App Profile</div>
+            <div>
+                <Header title="Food Ordering App" showSearchBox={false} history={this.props.history} restaurantSearchHandler={this.restaurantSearchHandler} />
+                <div style={{textAlign : 'center', padding : '20px 0px'}}>User Profile</div>
+            </div>
         )
     }
 }
-export default withStyles(styles) (Profile);
+export default withStyles(styles)(Profile);
